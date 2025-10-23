f0a375ac163875922d62f91cb177cce1fb79bd7c
SillyBee <3576527475a@gmail.com>
2025-10-24 01:01:13 +0800

    feat: finish tutorial minigrep

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
 use std::env;
-use std::fs;
-fn main(){
-    let args: Vec<String> = env::args().collect();
+use std::process;
 
-    let config = parse_config(&args);
+use minigrep::Config;
 
-    println!("Searching for '{}' in file '{}'", config.query, config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-    println!("With text:\n{}", contents);
+fn main() {
+
+
+   let config = Config::build(env::args()).unwrap_or_else(|err|  {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
 
 
-struct Config {
-    query: String,
-    file_path: String,
-}
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config { query, file_path }
-}
